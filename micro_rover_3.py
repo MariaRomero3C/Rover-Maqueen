768fdbaf1d1b1018b0a0e00cdc1b96d140ee12b1
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-04-19 13:40:25 +0200

    Create micro_rover_3.py

---
+++ micro_rover_3.py
@@ -0,0 +1,15 @@
+
+from microbit import *
+from MicroRover import *
+
+robot = Micro_Rover()
+
+while True:
+    distancia = robot.get_distance()
+    if distancia <= 10:
+        robot.motor(-55,-55)
+        sleep(500)
+        robot.motor(0,255)
+        sleep(1000)
+    else:
+        robot.motor(255,255)
