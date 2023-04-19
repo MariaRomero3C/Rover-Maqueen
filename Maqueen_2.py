f8fc499d4cf1a37895b7b2d59f51ec211cac126c
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-04-19 13:36:25 +0200

    Create Maqueen_2.py

---
+++ Maqueen_2.py
@@ -0,0 +1,23 @@
+# Imports go at the top
+from microbit import *
+from Maqueen import *
+
+robot = Maqueen()
+
+sensor_1 = robot.read_patrol (0)
+sensor_2 = robot.read_patrol (1)
+
+while True:
+    sensor_1 = robot.read_patrol (0)
+    sensor_2 = robot.read_patrol (1)
+    if sensor_1 == 0 and sensor_2 == 0:
+        robot.motor_stop_all()
+    elif sensor_1 == 1 and sensor_2 == 0:
+        robot.motor_stop_all()
+    elif sensor_1 == 0 and sensor_2 == 1:
+        robot.motor_stop_all()
+    elif sensor_1 == 1 and sensor_2 == 1:
+        robot.set_motor(0,255)
+        robot.set_motor(1,255)    
+    else:
+        display.show(Image.HEART)
