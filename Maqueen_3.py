5c20355df1b96e83ddbb02d6d71981c7e9770cc8
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-04-24 13:11:01 +0200

    Create Maqueen_3.py

---
+++ Maqueen_3.py
@@ -0,0 +1,12 @@
+# Imports go at the top
+from microbit import *
+from Maqueen import *
+
+robot = Maqueen()
+
+def andar_atras():
+    robot.set_motor(0, -255)
+    robot.set_motor(1, -255)
+
+while True:
+    andar_atras()
