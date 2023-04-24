61969898ad18ca9f6fb84c6ecb2dacb6da116b15
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-04-24 13:43:44 +0200

    Create Maqueen_5.py

---
+++ Maqueen_5.py
@@ -0,0 +1,22 @@
+from microbit import *
+from Maqueen import *
+
+robot = Maqueen()
+
+
+while True:
+    distancia = robot.read_distance()
+    if distancia <= 10:
+        robot.turn_left()
+        robot.forward()
+        sleep(500)
+        robot.turn_right()
+        robot.forward()
+        sleep(1000)
+        robot.turn_right()
+        robot.forward()
+        sleep(500)
+        robot.turn_left()
+        robot.forward()
+    else:
+        robot.forward()
