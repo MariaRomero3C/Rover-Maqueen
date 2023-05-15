52b9b7815d9c54c09398eebf08f03d31ca449ec0
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-05-15 13:45:59 +0200

    Create caballo.py

---
+++ caballo.py
@@ -0,0 +1,12 @@
+# Imports go at the top
+from microbit import *
+from maqueen import *
+
+robot = Maqueen()
+
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.girar_izquierda()
+robot.mover_celda()
+#hay que cambiar los segundos en la funcion movercelda
