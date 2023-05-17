5d0f893f7c8441b308b12cfbc08963268a6d0c0a
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-05-17 13:40:49 +0200

    Create Maqueen_10.py

---
+++ Maqueen_10.py
@@ -0,0 +1,18 @@
+# Imports go at the top
+from microbit import *
+from maqueen import *
+
+robot = Maqueen()
+
+robot.girar_derecha()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+#cambiar la funcion turnright de 255 a 170 y de -255 a 0
