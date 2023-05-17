2d2fcaf22268635c1ad1fc2e2e0b1ebb41e9d060
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-05-17 13:12:13 +0200

    Create Maqueen_8.py

---
+++ Maqueen_8.py
@@ -0,0 +1,14 @@
+# Imports go at the top
+from microbit import *
+from maqueen import *
+
+robot = Maqueen()
+
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
+robot.mover_celda()
