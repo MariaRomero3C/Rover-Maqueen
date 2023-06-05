fcd6d33612917c75667ef3c9d2ca07884cb44410
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-06-05 13:22:13 +0200

    Update Maqueen_2.py

--- Maqueen_2.py
+++ Maqueen_2.py
@@ -1,23 +1,22 @@
 # Imports go at the top
 from microbit import *
-from Maqueen import *
+from maqueen import *
 
 robot = Maqueen()
-
-sensor_1 = robot.read_patrol (0)
-sensor_2 = robot.read_patrol (1)
 
 while True:
     sensor_1 = robot.read_patrol (0)
     sensor_2 = robot.read_patrol (1)
-    if sensor_1 == 0 and sensor_2 == 0:
-        robot.motor_stop_all()
-    elif sensor_1 == 1 and sensor_2 == 0:
+    if sensor_1 == 1 and sensor_2 == 1:
         robot.motor_stop_all()
     elif sensor_1 == 0 and sensor_2 == 1:
-        robot.motor_stop_all()
-    elif sensor_1 == 1 and sensor_2 == 1:
-        robot.set_motor(0,255)
-        robot.set_motor(1,255)    
+        robot.set_motor(0,0)
+        robot.set_motor(1,100)
+    elif sensor_1 == 1 and sensor_2 == 0:
+        robot.set_motor(0,100)
+        robot.set_motor(1,0)
+    elif sensor_1 == 0 and sensor_2 == 0:
+        robot.set_motor(0,200)
+        robot.set_motor(1,200)    
     else:
         display.show(Image.HEART)
