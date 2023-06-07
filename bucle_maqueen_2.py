22c9eabd293f17d392d63bbe8ce2528c8e9187de
MariaRomero3C <114919286+MariaRomero3C@users.noreply.github.com>
2023-06-07 13:10:54 +0200

    Create bucle_maqueen_2.py

---
+++ bucle_maqueen_2.py
@@ -0,0 +1,13 @@
+# Imports go at the top
+from microbit import *
+from maqueen import *
+
+robot = Maqueen()
+
+x = 0
+
+while x<3:
+    robot.forward()
+    microbit.sleep(500)
+    robot.motor_stop_all()
+    x = x + 1
